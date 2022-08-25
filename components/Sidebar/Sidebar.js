d744f302c4fb6fa48e809c20e6bd8b99c0194539
Your Namefotostoys <ciel2000@gmail.com>
2022-08-25 14:47:50 -0300

    link

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -1,17 +1,24 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__container">
-        
 
         <div className="sidebar__bottom">
-          <Image src="/bodie.png" alt="avatar" width="50" height="50" />
-          <h2>Bodie</h2>
+
+          <Link href="https://bacode.com.ar/" passHref>
+            <a target='_blank' >
+
+              <Image src="/bodie.png" alt="avatar" width="50" height="50" />
+              <h2>Bodie</h2>
+            </a>
+          </Link>
+
         </div>
-        
+
 
       </div>
     </div>
